63def5de9799812f49f7c8b95222b2c1e95658b4
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-09-26 07:41:14 +0600

    first commit

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Countries.css'
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countries is other file!!</h2>
+            <nav className='menu'>
+                <a href="/home">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/about">About</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Countries;
